3604f3f43c3090ac4e7f2defbac3c3300a26cd29
gian.boza@tecsup.edu.pe <gian.boza@tecsup.edu.pe>
2025-05-11 18:16:53 -0500

    Actualización de la configuración para la base de datos remota

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,12 +1,9 @@
-import dotenv from 'dotenv';
-dotenv.config();  // Cargar las variables de entorno desde .env
-
 export default {
-  HOST: process.env.DB_HOST || "localhost",  // Usar la variable de entorno DB_HOST o "localhost" por defecto
-  USER: process.env.DB_USER || "root",      // Usar la variable de entorno DB_USER o "root" por defecto
-  PASSWORD: process.env.DB_PASSWORD || "",  // Usar la variable de entorno DB_PASSWORD o vacío por defecto
-  DB: process.env.DB_NAME || "jwt_auth_db",  // Usar la variable de entorno DB_NAME o "jwt_auth_db" por defecto
-  dialect: process.env.DB_DIALECT || "mysql", // Usar la variable de entorno DB_DIALECT o "mysql" por defecto
+  HOST: "aws.connectdb.cloud", // ejemplo
+  USER: "andre_user",
+  PASSWORD: "claveSegura123",
+  DB: "jwt_auth_db",
+  dialect: "mysql", // o "postgres"
   pool: {
     max: 5,
     min: 0,
